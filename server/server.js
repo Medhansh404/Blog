49a0a851e2196bdebfd0f3a9d02e5ad4a35e815e
Medhansh404 <21ucs126@lnmiit.ac.in>
2024-04-11 23:43:34 +0530

    1.3 db connection and apis

--- server/server.js
+++ server/server.js
@@ -1,13 +1,71 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
+const db = require("./db");
+
 app.use(cors())
 app.use(express.json())
+app.use(morgan("dev"))
 
-app.post("/", 
-(req, res) =>
-{
-const id = req.body.id
-console.log(req.body)
-res.status(200).json({message:'This is the blog content for blog { id }'})})
+
+
+// retrieve all the posts blogs
+app.get("/api/v1/blogs", async (req, res)=>{
+    try{
+    const objs = await db.query("select * from blogs");
+    console.log(objs)
+    res.status(200).json({
+        status: "success",
+        results: objs.rows.length,
+        data:{
+            posts: objs.rows
+        },
+    });
+}
+catch(err){
+    console.log(err);
+}
+});
+
+
+// retrieve only one post
+app.get("/api/v1/blogs/:id", async (req, res)=>{
+    const post_id = req.params.id;
+    try{
+        const objs = await db.query(`select * from blogs where id = $1`, [post_id]);
+        console.log(objs)
+        res.status(200).json({
+            status: "success",
+            results: objs.rows.length,
+            data:{
+                posts: objs.rows
+            },
+        });
+    }
+    catch(err){
+        console.log(err);
+    }
+    });
+
+//create post
+app.post("/api/v1/blogs", async (req, res)=>{
+    try{
+        const obj = await db.query("insert into blogs ()")
+    }
+    catch(err){}
+    
+})
+
+//update post
+app.put("/api/v1/blogs/:id", (req, res)=>{
+
+})
+
+//delete post
+app.delete("/api/v1/blogs/:id", (req, res)=>{
+
+})
+
 app.listen(process.env.PORT||3000, ()=>console.log("Server running"));
